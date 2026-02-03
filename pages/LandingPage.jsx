ec88d2b18cfea148557f2a6f0df43d47478a7685
nonydev <karldjansi123@gmail.com>
2026-02-03 02:12:11 +0000

    added tailwindcss

--- pages/LandingPage.jsx
+++ pages/LandingPage.jsx
@@ -1,9 +1,10 @@
-export default function landingPage(){
+export default function LandingPage(){
     return(
-        <div className="container landing-page">
+        <div className="container landing-page flex bg-primary">
             <div>
                 <h1>WordFlow</h1>
                 <p>Where ideas synchronize in real-time. A high-performance collaborative workspace designed for seamless document engineering.</p>
+                
             </div>
         </div>
     )
