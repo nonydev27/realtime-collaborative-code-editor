ecf0e12c8bef949b057dc33a118e69f2e1d34893
nonydev <karldjansi123@gmail.com>
2026-02-03 03:21:57 +0000

    ...

--- pages/LandingPage.jsx
+++ pages/LandingPage.jsx
@@ -30,7 +30,7 @@ export default function LandingPage() {
                 </button>
 
                      <button 
-                    className="mt-8 px-8 py-3 m-2 font-bold transition-transform hover:scale-105"
+                    className="mt-8 px-8 py-3 m-2 font-bold transition-transform hover:scale-105 capitalize"
                     style={{ backgroundColor: 'var(--primary)', color: 'white' }}
                 >
                    create document
