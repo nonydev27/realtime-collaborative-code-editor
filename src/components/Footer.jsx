b301dc9161cc628ed84c61bc69db5a580877d0f1
nonydev <karldjansi123@gmail.com>
2026-02-03 02:33:15 +0000

    ..

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,14 @@ export default function Footer(){
     return(
 
        <footer className="py-8 text-center opacity-60 text-sm">
+
   <p>© {new Date().getFullYear()} WordFlow. Built with React & WebSockets.</p>
+
   <p className="mt-1">
     Developed by <span className="font-bold text-[--tertiary]">Karl Djansi</span> 
     • Mentored by <a href="https://github.com/KelvCodes" className="hover:underline">KelvCodes</a>
   </p>
+
 </footer>
     )
 }
