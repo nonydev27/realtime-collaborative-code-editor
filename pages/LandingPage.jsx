5444787379243a045cba355b0296c4befb80c224
nonydev <karldjansi123@gmail.com>
2026-02-03 05:15:17 +0000

    ...

--- pages/LandingPage.jsx
+++ pages/LandingPage.jsx
@@ -37,8 +37,8 @@ export default function LandingPage() {
                    create document
                 </button>
 
-                <button className="rounded bg-sky-600 px-4 py-2 text-sm text-white data-active:bg-sky-700 data-hover:bg-sky-500">
-
+                <button data-hover className="rounded bg-sky-600 px-4 py-2 text-sm text-white data-active:bg-sky-700 data-hover:bg-sky-500">
+                    Hello
                 </button>
             </div>
 
