55b0e1d90d7dee387d41426f420483b0a5da4cdb
nonydev <karldjansi123@gmail.com>
2026-02-06 16:07:06 +0000

    ...

--- pages/NotFound.jsx
+++ pages/NotFound.jsx
@@ -1,13 +1,13 @@
 export default function NotFound(){
     return(
         <>
-            <main className="h-screen w-full text-5xl p-6 flex flex-col justify-center">
+            <main className="h-screen w-full p-6 flex flex-col justify-center">
                 
             <div className="">
                 <h2 className="text-9xl">Error 404</h2>
             </div>
 
-                <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis suscipit dolores eos sint nihil autem beatae blanditiis minima inventore consectetur ex animi, numquam dolore neque accusamus fugit error ipsam maiores.</p>
+                <p className="text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis suscipit dolores eos sint nihil autem beatae blanditiis minima inventore consectetur ex animi, numquam dolore neque accusamus fugit error ipsam maiores.</p>
             </main>
 
         </>
