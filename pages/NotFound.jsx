8b28cb22d17bc7d9bb02a60b16ae68e36d5ad065
nonydev <karldjansi123@gmail.com>
2026-02-06 16:06:43 +0000

    ...

--- pages/NotFound.jsx
+++ pages/NotFound.jsx
@@ -7,7 +7,7 @@ export default function NotFound(){
                 <h2 className="text-9xl">Error 404</h2>
             </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis suscipit dolores eos sint nihil autem beatae blanditiis minima inventore consectetur ex animi, numquam dolore neque accusamus fugit error ipsam maiores.</p>
+                <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis suscipit dolores eos sint nihil autem beatae blanditiis minima inventore consectetur ex animi, numquam dolore neque accusamus fugit error ipsam maiores.</p>
             </main>
 
         </>
