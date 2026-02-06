2c1786fb1899f702ef1ca5e3f649fcea4e6de5d2
nonydev <karldjansi123@gmail.com>
2026-02-06 16:20:01 +0000

    ...

--- pages/NotFound.jsx
+++ pages/NotFound.jsx
@@ -1,13 +1,16 @@
+import Footer from "../src/components/Footer"
 export default function NotFound(){
     return(
         <>
-            <main className="h-screen w-full p-24 flex flex-col justify-center">
+            <main className=" overflow-hidden w-full p-30 flex flex-col justify-center">
                 
             <div className="">
                 <h2 className="text-9xl">Error 404</h2>
                 <p className="text-3xl"> Oops! We can't seem to find the page you're looking for. The page might have either been taken down or broken.</p>
            </div>
             </main>
+
+            <Footer/>
 
         </>
     )
